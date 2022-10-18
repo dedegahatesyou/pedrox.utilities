f22763dac625023108f83d8dbc7b6202b7fcc846
dedegahatesyou <96036855+dedegahatesyou@users.noreply.github.com>
2022-10-17 21:43:20 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,10 @@
+--[[
+
+pedrox.utilities
+very good script for many games.
+😝😝😝😝 ggs
+
+--]]
+
+
+loadstring(game:HtppGet("https://raw.githubusercontent.com/dedegahatesyou/pedrox.utilities/main/Other/hub.lua",true))()
